054bff975a01307dd28dc7cb1d05ab9f7a56e400
Naveen <naveenvaddarapu4@gmail.com>
2024-05-04 15:53:03 +0530

    Git Commands to Pull the code

--- src/test/java/Git_Commands/GitCommands.java
+++ src/test/java/Git_Commands/GitCommands.java
@@ -1,5 +1,10 @@
 package Git_Commands;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GitCommands {
 	
 //	1) Initialize the folder as Git repository - git init
@@ -13,16 +18,27 @@ public class GitCommands {
 	// git remote add origin <server url>
 	
 //	push the code to the Github Repository -  git push origin master.
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
+	
+//	The below link is the confluence page for the GIT Commands
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html"); //Git Commands Confluence Page
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }

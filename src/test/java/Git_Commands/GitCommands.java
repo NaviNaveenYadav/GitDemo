7d1a97f0ce2f73812da1ec673a7df5c274bcd394
Naveen <naveenvaddarapu4@gmail.com>
2024-05-04 11:46:13 +0530

    Added Git Commands File

---
+++ src/test/java/Git_Commands/GitCommands.java
@@ -0,0 +1,28 @@
+package Git_Commands;
+
+public class GitCommands {
+	
+//	1) Initialize the folder as Git repository - git init
+	
+//	2) Add the files to staging - git add *
+	// Here the "*" means to add all the files to the staging
+	
+//	3) Commit the files (which are added to the staging) to the Git - git commit -m "Message"
+	
+//	4)Connect your local repository to the remote server. use the below command if it is for the first time
+	// git remote add origin <server url>
+	
+//	push the code to the Github Repository -  git push origin master.
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+}
